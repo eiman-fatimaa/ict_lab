d4c3230a22e711fbbcec3623ea8e4219d627890f
Eiman Fatima <ranafuntime@gmail.com>
2025-12-05 15:44:33 +0500

    Add three Python function files

---
+++ add_numbers.py
@@ -0,0 +1,6 @@
+def add_nums(a, b):
+    sumNums = a + b
+    return sumNums
+
+result = add_nums(5, 3)
+print(result)
---
+++ multiply.py
@@ -0,0 +1,4 @@
+def product(a,b):
+    return a * b
+prod = product(2,3)
+print(prod)
