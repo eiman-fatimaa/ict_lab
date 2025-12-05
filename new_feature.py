6b04fec56cf896c77838e5d71283440de7a3ef38
Eiman Fatima <ranafuntime@gmail.com>
2025-12-05 16:02:07 +0500

    Add new feature file

---
+++ new_feature.py
@@ -0,0 +1,5 @@
+def birthday_wish(name, age):
+    print ("Dear",name, ", Happy", age, "th Birthday!")
+name_in = input("What is your name?")
+age_in = input("How old are you?")
+birthday_wish(name_in, age_in)
